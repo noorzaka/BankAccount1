051ffc649a4a561b29bc82653d26ab10fc153836
noorzaka <nooralhuda101020@gmail.com>
2024-06-22 23:39:00 +0300

    adding Transfar feature

--- BankAccount.java
+++ BankAccount.java
@@ -37,6 +37,36 @@ public class BankAccount {
     public String getAccountHolder() {
         return accountHolder;
     }
+  public void transfer(BankAccount toAccount, double amount) {
+        if (amount > this.balance) {
+            System.out.println("Insufficient funds");
+        } else {
+            this.withdraw(amount);
+
+            double convertedAmount = convertCurrency(amount, this.currency, toAccount.getCurrency());
+            toAccount.deposit(convertedAmount);
+        }
+    private double convertCurrency(double amount, String fromCurrency, String toCurrency) {
+        
+        double conversionRate = 1.0;
+        if (fromCurrency.equals("USD") && toCurrency.equals("EUR")) {
+            conversionRate = 0.85;
+        } else if (fromCurrency.equals("USD") && toCurrency.equals("GBP")) {
+            conversionRate = 0.75;
+        } else if (fromCurrency.equals("EUR") && toCurrency.equals("USD")) {
+            conversionRate = 1.18;
+        } else if (fromCurrency.equals("EUR") && toCurrency.equals("GBP")) {
+            conversionRate = 0.88;
+        } else if (fromCurrency.equals("GBP") && toCurrency.equals("USD")) {
+            conversionRate = 1.33;
+        } else if (fromCurrency.equals("GBP") && toCurrency.equals("EUR")) {
+            conversionRate = 1.14;
+        } else if (toAccount.getCurrency().equals("KMF")) {
+            conversionRate = 0.005; // Example conversion rate for Comorian Franc
+        }
+        return amount * conversionRate;
+    }
+    }
 
 
 }
