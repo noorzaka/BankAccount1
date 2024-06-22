9070ba89a3c69cde4ff08b50286fb1aeff7e1045
noorzaka <105720309+noorzaka@users.noreply.github.com>
2024-06-22 16:20:39 +0300

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,36 @@
+
+public class BankAccount {
+    private String accountHolder;
+    private double balance;
+
+    
+    public BankAccount(String accountHolder, double initialBalance) {
+        this.accountHolder = accountHolder;
+        this.balance = initialBalance;
+    }
+  
+
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            this.balance += amount;
+        } else {
+            throw new IllegalArgumentException("Deposit amount must be positive.");
+        }
+    }
+ public void withdraw(double amount) {
+      
+            balance -= amount;
+        
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+
+}
